48e6931433429439cedf52fce7bbb65890f531b9
Sumeet Kakkar <sumeetkakkar@gmail.com>
2015-05-04 13:03:55 -0700

    sslify

--- sampleapp/settings.py
+++ sampleapp/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'sslify.middleware.SSLifyMiddleware',
 )
 
 ROOT_URLCONF = 'sampleapp.urls'
